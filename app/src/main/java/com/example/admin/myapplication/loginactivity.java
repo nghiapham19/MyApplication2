d3f24822579c358e71fce6506fc28737992a5e10
nghiapham19 <nghiaphamgame@gmail.com>
2017-10-28 14:23:42 +0700

    vua thay doi

---
+++ app/src/main/java/com/example/admin/myapplication/loginactivity.java
@@ -0,0 +1,10 @@
+package com.example.admin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 28-10-2017.
+ */
+
+public class loginactivity extends AppCompatActivity {
+}
